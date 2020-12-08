058b3fd8d2f21f62eaa8c6e514d58eb136a23bf1
ash-old <ashley.oldham@gmail.com>
2020-12-08 00:19:19 +0000

    added assets to project, and imported main image

--- src/Components/LaunchDetail.js
+++ src/Components/LaunchDetail.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import RocketImage from './RocketImage';
 
 const LaunchDetail = (props) => {
 
     const detail = props.launches.map(launch => {
-        return <p key={launch.flight_number}>{launch.mission_name}</p>
+    return <p key={launch.flight_number}>{launch.mission_name}</p>
     })
 
     return(
         <div>
+            <RocketImage />
             {detail}
         </div>
     )
---
+++ src/Components/RocketImage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import launch from '../assets/img/launch-home.png';
+
+function RocketImage() {
+
+    return (
+        <div>
+            <img src={launch} alt="space x rocket" height={200} width={300} />
+        </div>
+    )
+
+}
+
+
+export default RocketImage;
