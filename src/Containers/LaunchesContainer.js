c9f52a2365b2131fc11d571ca277e3ecae78c706
ash-old <ashley.oldham@gmail.com>
2020-12-09 22:25:14 +0000

    proptypes added on container

--- src/Containers/LaunchesContainer.js
+++ src/Containers/LaunchesContainer.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import LaunchDetail from '../Components/LaunchDetail';
 
 
@@ -37,7 +38,10 @@ class LaunchesContainer extends Component {
 
 
 
-
 }
+
+LaunchesContainer.propTypes = {
+    launches: PropTypes.string.isRequired
+};
 
 export default LaunchesContainer;
